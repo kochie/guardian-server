9db03ec6e1f07c9bcd01de80c5091df755eb9e6e
Robert Koch <robert@kochie.io>
2018-01-10 15:23:26 +1100

    Added Gopkg and updated docker.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,17 @@
 FROM golang:alpine
+ARG release_version="0.3.2"
 
-CMD ["go "]
+RUN apk add --update curl git
+
+RUN curl -fsSL -o /usr/local/bin/dep https://github.com/golang/dep/releases/download/v${release_version}/dep-linux-amd64 && chmod +x /usr/local/bin/dep
+
+RUN mkdir -p /go/src/github.com/kochie
+WORKDIR /go/src/github.com/kochie
+
+COPY . .
+
+RUN dep ensure
+
+RUN go-wrapper install
+
+CMD ["go-wrapper", "run"]
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"database/sql"
+//	"database/sql"
 	_ "github.com/lib/pq"
 
-	"./handlers/github"
-	"./handlers/user"
+//	"./handlers/github"
+//	"./handlers/user"
 )
 
 func main() {
@@ -15,17 +15,17 @@ func main() {
 
 	addRoutes()
 
-	github.CreateConnection()
+//	github.CreateConnection()
 
 	fmt.Println("Server listening on port " + port[1:])
 	http.ListenAndServe(port, nil)
 }
 
 func addRoutes() {
-	http.HandleFunc("/api/login/", user.Login)
-	http.HandleFunc("/api/logout/", user.Logout)
+//	http.HandleFunc("/api/login/", user.Login)
+//	http.HandleFunc("/api/logout/", user.Logout)
 
-	http.HandleFunc("/api/github/connect/", github.Connect)
-	http.HandleFunc("/api/github/disconnect/", github.Disconnect)
-	http.HandleFunc("/api/github/addkey/", github.Addkey)
+//	http.HandleFunc("/api/github/connect/", github.Connect)
+//	http.HandleFunc("/api/github/disconnect/", github.Disconnect)
+//	http.HandleFunc("/api/github/addkey/", github.Addkey)
 }
