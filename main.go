d3f7229adb27c7c34effe320c13660a0ef2ce0f5
Robert Koch <robert@kochie.io>
2018-01-14 22:44:45 +1100

    Changed to monogodb

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-//	"database/sql"
-	_ "github.com/lib/pq"
 
-//	"./handlers/github"
-//	"./handlers/user"
+	"gopkg.in/mgo.v2"
+	//	"./handlers/github"
+	//	"./handlers/user"
 )
 
 func main() {
@@ -15,17 +15,33 @@ func main() {
 
 	addRoutes()
 
-//	github.CreateConnection()
-
+	//	github.CreateConnection()
+	setup()
 	fmt.Println("Server listening on port " + port[1:])
 	http.ListenAndServe(port, nil)
 }
 
 func addRoutes() {
-//	http.HandleFunc("/api/login/", user.Login)
-//	http.HandleFunc("/api/logout/", user.Logout)
+	//	http.HandleFunc("/api/login/", user.Login)
+	//	http.HandleFunc("/api/logout/", user.Logout)
 
-//	http.HandleFunc("/api/github/connect/", github.Connect)
-//	http.HandleFunc("/api/github/disconnect/", github.Disconnect)
-//	http.HandleFunc("/api/github/addkey/", github.Addkey)
+	//	http.HandleFunc("/api/github/connect/", github.Connect)
+	//	http.HandleFunc("/api/github/disconnect/", github.Disconnect)
+	//	http.HandleFunc("/api/github/addkey/", github.Addkey)
+}
+
+func setup() {
+	session := newDatabaseConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(rows)
+}
+
+func newDatabaseConnection() (session *mgo.Session) {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
 }
